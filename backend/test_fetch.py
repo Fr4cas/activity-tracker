6868c1c485020fe60f46fdb06e864566f095dd7f
Lucas Lin <chenghsin2007@gmail.com>
2025-07-02 11:12:39 +0800

    test(fetch): test commit date extraction from PushEvents

--- backend/test_fetch.py
+++ backend/test_fetch.py
@@ -4,5 +4,10 @@ username = "Fr4cas"
 
 activity = fetch_github_activity(username)
 
-for event in activity:
-    print(event)
+# for event in activity:
+#     print(event)
+
+commit_dates = [event["created_at"][:10] for event in activity]
+
+print("Commit dates:")
+print(commit_dates)
