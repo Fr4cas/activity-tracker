0c0f5c3450e0a5d85c846e1e89326a58c6f146e1
Lucas Lin <chenghsin2007@gmail.com>
2025-07-02 09:58:10 +0800

    feat(fetch): filter and return only PushEvent activity

--- backend/fetch_github.py
+++ backend/fetch_github.py
@@ -1,5 +1,6 @@
 import requests
 
+# Get event data from user profile
 def fetch_github_activity(username):
     url = f"https://api.github.com/users/{username}/events/public"
     headers = {"Accept": "application/vnd.github.v3+json"}
@@ -7,7 +8,10 @@ def fetch_github_activity(username):
     try:
         response = requests.get(url, headers=headers)
         response.raise_for_status()
-        return response.json() 
+        return response.json()
+    
+        push_events = [event for event in events if event["type"] == "PushEvent"]
+        return push_events
 
     except requests.exceptions.RequestException as e:
         print("Error fetching GitHub data:", e)
