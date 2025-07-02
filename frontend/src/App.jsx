c394c24b7a2b13375ab9eda8f372bcbfb798b173
Lucas Lin <chenghsin2007@gmail.com>
2025-07-02 13:54:05 +0800

    feat(frontend): initialize React app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>GitHub Activity Tracker</h1>
+      <p>This app will display daily commit activity from a GitHub user.</p>
+    </div>
+  );
+}
+
+export default App;
